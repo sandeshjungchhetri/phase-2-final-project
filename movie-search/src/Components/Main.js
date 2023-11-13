bb2cd18e5889af387d69697b57a3dbec817f85c6
Sandesh Jung Chhetri <sandeshjung21@gmail.com>
2023-11-13 17:15:41 +1100

    Setting conditional statements for the movies

--- movie-search/src/Components/Main.js
+++ movie-search/src/Components/Main.js
@@ -5,3 +5,35 @@ import Card from "./Card";
 let API_key="&api_key=db95773a7fb212ba790d71f6adac0e7e";
 let base_url="https://api.themoviedb.org/3";
 let url=base_url+"/discover/movie?sort_by=popularity.desc"+API_key;
+let arr=["Popular","Theatre","Kids","Drama","Comedy"];
+const Main=()=>{
+    const [movieData,setData]=useState([]);
+    const [url_set,setUrl]=useState(url);
+    const [search,setSearch]=useState();
+    useEffect(()=>{
+        fetch(url_set).then(res=>res.json()).then(data=>{
+            setData(data.results);
+        });
+    },[url_set])
+    const getData=(movieType)=>{
+        if(movieType=="Popular")
+        {
+            url=base_url+"/discover/movie?sort_by=popularity.desc"+API_key;
+        }
+        if(movieType=="Theatre")
+        {
+            url=base_url+"/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22"+API_key;
+        }
+        if(movieType=="Kids")
+        {
+            url=base_url+"/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc"+API_key;
+        }
+        if(movieType=="Drama")
+        {
+            url=base_url+"/discover/movie?with_genres=18&primary_release_year=2014"+API_key;
+        }
+        if(movieType=="Comedy")
+        {
+            url=base_url+"/discover/movie?with_genres=35&with_cast=23659&sort_by=revenue.desc"+API_key;
+        }
+        setUrl(url);

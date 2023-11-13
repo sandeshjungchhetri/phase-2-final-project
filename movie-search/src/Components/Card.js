6db62364df175abb7839774558fafa597be74084
Sandesh Jung Chhetri <sandeshjung21@gmail.com>
2023-11-13 17:41:27 +1100

    Setting the return value

--- movie-search/src/Components/Card.js
+++ movie-search/src/Components/Card.js
@@ -1,3 +1,9 @@
 import react from "react";
 
-const Card=(movie)=>{
+const Card=(movie)=>{ 
+let img_path="https://image.tmdb.org/t/p/w500";
+return(
+    <>
+        <div className="movie">
+            <img src={img_path+movie.info.poster_path} className="poster"></img>
+            <div className="movie-details">

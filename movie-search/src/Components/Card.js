5a569283d8a7cea35935838bb5f13aa189312e74
Sandesh Jung Chhetri <sandeshjung21@gmail.com>
2023-11-13 17:43:25 +1100

    Setting the return value of average score

--- movie-search/src/Components/Card.js
+++ movie-search/src/Components/Card.js
@@ -7,3 +7,7 @@ return(
         <div className="movie">
             <img src={img_path+movie.info.poster_path} className="poster"></img>
             <div className="movie-details">
+            <div className="box">
+                        <h4 className="title">{movie.info.title}</h4>
+                        <p className="rating">{movie.info.vote_average}</p>
+            </div>
